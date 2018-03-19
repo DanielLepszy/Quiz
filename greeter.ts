c58482da94076d7af4eb35200ff6ebe9f763f97e
Daniel Lepszy <daniel.lepszy@gmail.com>
2018-03-19 11:13:56 +0100

    First commit in branch work

---
+++ greeter.ts
@@ -0,0 +1,14 @@
+class Customer {
+    public CustomerName :
+    string ="";
+    Validate()
+    {
+        alert('test');
+    }
+}
+class main extends Customer
+{
+    Validate(){
+    alert('dziala');
+    }
+}
